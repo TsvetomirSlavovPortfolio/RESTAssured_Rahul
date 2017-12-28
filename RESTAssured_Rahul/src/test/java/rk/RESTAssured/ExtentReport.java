873d1d3bc27ba8985519a5e429ef7d1d4b1d9f73
CaptainKing <CaptainKing@CAPTAIN>
2017-12-28 23:03:35 +0530

    Adding Extent Report

--- RESTAssured_Rahul/src/test/java/rk/RESTAssured/ExtentReport.java
+++ RESTAssured_Rahul/src/test/java/rk/RESTAssured/ExtentReport.java
@@ -1,17 +1,10 @@
 package rk.RESTAssured;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.junit.Assert;
 import org.testng.ITestResult;
-import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+ 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -20,100 +13,59 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
-
-public class ExtentReport 
+ 
+public class ExtentReport
 {
-	public static ExtentHtmlReporter htmlReporter=null;
-	public static ExtentReports extent=null;
-	public static ExtentTest test=null;
+	//http://automationtesting.in/extent-report-with-multiple-classes/
+	//http://automationtesting.in/capture-fullpage-screenshot-in-extent-reports-java/
+	//http://automationtesting.in/capture-screenshot-in-extent-reports-java-version3/
+	//http://automationtesting.in/generating-extent-reports-java-version3/
 	
-/*	public  ExtentReport(ExtentHtmlReporter htmlReporter, ExtentReports extent, ExtentTest test)
-	{
-		ExtentReport.htmlReporter = htmlReporter;
-		ExtentReport.extent = extent;
-		ExtentReport.test = test;
-	}*/
-	
-	@BeforeTest
-	public static ExtentReports startReport()
-	{
-		DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
-		Date date = new Date();	
-
-		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"/src/test/resources/ExtentReports/ExtentReport_Logs_"+dateFormat.format(date)+".html");
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		extent.setSystemInfo("OS Info", "Windows 10");
-		extent.setSystemInfo("User Name", "Rahul Kinge");
-		extent.setSystemInfo("Host Name", "Swati Kinge");
-		
-		htmlReporter.config().setDocumentTitle("Automation Extent Report by Rahul Kinge");
-		htmlReporter.config().setReportName("My Own Automation Extent Report");
-		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
-		htmlReporter.config().setTheme(Theme.DARK);
-		return extent;
-	}
-
-	@Test
-	public void logGernaration()
-	{
-		test = extent.createTest("This test will create logs for the test case");
-		test.log(Status.INFO, "startReport() Method will return ExtentReports() object");
-		test.log(Status.INFO,"I am in actual test case");
-		test.log(Status.INFO, "We can write actual code logic here in this test case");
-		
-		test.info(MarkupHelper.createLabel("------XX  Using Labels  XX------", ExtentColor.RED));
-		test.info(MarkupHelper.createLabel("This is Test Logger 1", ExtentColor.BLUE));
-		test.info(MarkupHelper.createLabel("This is Test Logger 2", ExtentColor.BLUE));
-		test.info(MarkupHelper.createLabel("This is Test Logger 3", ExtentColor.BLUE));
-		test.info(MarkupHelper.createLabel("This is Test Logger 4", ExtentColor.BLUE));
-		test.info(MarkupHelper.createLabel("This is Test Logger 5", ExtentColor.BLUE));
-		test.info(MarkupHelper.createLabel("This is Test Logger 6", ExtentColor.BLUE));
-	}
-	@Test
-	public void demoTestPass()
-	{
-		test = extent.createTest("demoTestPass","This test will demonstrate the PASS test case");
-		Assert.assertTrue(true);
-	}
-	
-	@Test
-	public void demoTestFail()
-	{
-		test = extent.createTest("demoTestFail","This test will demonstrate the FAIL test case");
-		Assert.assertTrue(false);
-	}
-	
-	@Test
-	public void demoTestSkip()
-	{
-		test = extent.createTest("demoTestSkip","This test will demonstrate the SKIP test case");
-		throw new SkipException("This test not is ready for execution");
-	}
-	
-	@AfterMethod
-	public void getResult(ITestResult result)
-	{
-		if(result.getStatus()== ITestResult.FAILURE)
-		{
-			test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+ "Test Case Failed due to below issues", ExtentColor.RED));
-			test.fail(result.getThrowable());
-		}
-		else if(result.getStatus()== ITestResult.SUCCESS)
-		{
-			test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+ "Test Case is SUCCESS", ExtentColor.GREEN));
-		}
-		else
-		{
-			test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+ "Test Case SKIPPED", ExtentColor.YELLOW));
-			test.fail(result.getThrowable());
-		}
-	}
-	
-	@AfterTest
-	public static void tearDown()
-	{
-		extent.flush();
-	}
+    public static ExtentHtmlReporter htmlReporter;
+    public static ExtentReports extent;
+    public static ExtentTest test;
+     
+    @BeforeSuite
+    public void setUp()
+    {
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/test-output/MyOwnReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+         
+        extent.setSystemInfo("OS", "Mac Sierra");
+        extent.setSystemInfo("Host Name", "Krishna");
+        extent.setSystemInfo("Environment", "QA");
+        extent.setSystemInfo("User Name", "Krishna Sakinala");
+         
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("AutomationTesting.in Demo Report");
+        htmlReporter.config().setReportName("My Own Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.DARK);
+    }
+     
+    @AfterMethod
+    public void getResult(ITestResult result)
+    {
+        if(result.getStatus() == ITestResult.FAILURE)
+        {
+            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" Test case FAILED due to below issues:", ExtentColor.RED));
+            test.fail(result.getThrowable());
+        }
+        else if(result.getStatus() == ITestResult.SUCCESS)
+        {
+            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" Test Case PASSED", ExtentColor.GREEN));
+        }
+        else
+        {
+            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" Test Case SKIPPED", ExtentColor.ORANGE));
+            test.skip(result.getThrowable());
+        }
+    }
+     
+    @AfterSuite
+    public void tearDown()
+    {
+        extent.flush();
+    }
 }
--- RESTAssured_Rahul/src/test/java/rk/RESTAssured/GroovyFeatures.java
+++ RESTAssured_Rahul/src/test/java/rk/RESTAssured/GroovyFeatures.java
@@ -6,45 +6,39 @@ import static io.restassured.path.json.JsonPath.*;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-
 import java.util.List;
 import java.util.Map;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 
-public class GroovyFeatures 
+public class GroovyFeatures extends ExtentReport
 {	
 	//  http://james-willett.com/2015/06/rest-assured-extract-json-response/
 	//  http://james-willett.com/2017/05/rest-assured-gpath-json/
-	public static ExtentReports extent=ExtentReport.startReport();
-	public static ExtentTest test=null;
+/*	public static ExtentReports extent=null;
+	public static ExtentTest test=null;*/
 	
-/*	public GroovyFeatures(ExtentHtmlReporter htmlReporter, ExtentReports extent, ExtentTest test)
-	{
-		super(htmlReporter, extent, test);
-	}*/
-
 	@BeforeClass
 	public void setUPREstAssured()
 	{
+		test = extent.createTest("set UP REST Assured");
 	    RestAssured.baseURI = "http://services.groupkt.com";
 	    RestAssured.basePath = "/country/";
 /*	    RestAssured.port = 80;
 	    RestAssured.authentication = basic("username", "password");
 	    RestAssured.rootPath = "store.book";*/
+	    
 	}
 
 	//Example 1 – Use path() on Response
 	@Test(groups = { "Groovy", "Search_Text_Lands" },priority=1)
 	public void testPathOnResponse()
 	{
-		test = extent.createTest("This test will create logs for the test case");
-		test.log(Status.INFO,"I am in testPathOnResponse test case");
+		test = extent.createTest("Create Test testPathOnResponse");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in testPathOnResponse test case", ExtentColor.BLUE));
 		Response resp = get("search?text=lands");
 		System.out.println(resp.path("RestResponse.result.name"));
 		System.out.println(resp.path("RestResponse.result.name[0]"));  //find the first “name”
@@ -58,13 +52,14 @@ public class GroovyFeatures
         
         List<Map<String, String>> companies = resp.jsonPath().getList("RestResponse.result");
         System.out.println(companies.get(0).get("name"));			   //find the first “name”
-        ExtentReport.tearDown();
 	}
 
 	//Example 2 – Specify JSONPath
 	@Test (groups = { "Groovy", "Search_Text_Lands" },priority=2)
 	public void testJSONPathOnResponse()
 	{
+		test = extent.createTest("Create Test testJSONPathOnResponse");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in testJSONPathOnResponse test case", ExtentColor.BLUE));
 		Response resp = get("search?text=lands");
 		JsonPath jsonPath = new JsonPath(resp.asString());
 		System.out.println(jsonPath.get("RestResponse.result.name"));
@@ -79,7 +74,9 @@ public class GroovyFeatures
 	//Example 3 – Response as String 
 	@Test(groups = { "Groovy", "Search_Text_Lands" },priority=3)
 	public void testStringOnResponse()
-	{		
+	{	
+		test = extent.createTest("Create Test testStringOnResponse");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in testStringOnResponse test case", ExtentColor.BLUE));
 	    String responseAsString = get("search?text=lands").asString();
 	    System.out.println(JsonPath.from(responseAsString).get("RestResponse.result.name"));
 	}
@@ -95,6 +92,8 @@ public class GroovyFeatures
 	@Test(groups = { "Groovy", "Search_Text_Lands", "Football" },priority=3)
 	public void testLengthOfResponse()
 	{
+		test = extent.createTest("Create Test testLengthOfResponse");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in testLengthOfResponse test case", ExtentColor.BLUE));
 		given().
 		get("search?text=lands").
 		then().
@@ -109,6 +108,8 @@ public class GroovyFeatures
 	@Test (groups = { "Groovy", "Search_Text_Lands" },priority=3)
 	public void extractMapOfObjectWithFindAllAndFind() 
 	{
+		test = extent.createTest("Create Test extractMapOfObjectWithFindAllAndFind");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in extractMapOfObjectWithFindAllAndFind test case", ExtentColor.BLUE));
 	    Response response = get("http://api.football-data.org/v1/teams/66/players");
 	    Map<String,?> playerOfCertainPosition = response.path("players.findAll { it.position == \"Centre-Back\" }.find { it.nationality == \"Argentina\" }");
 	    System.out.println(playerOfCertainPosition);
@@ -116,7 +117,10 @@ public class GroovyFeatures
 	
 	// Use Parameters in your GPath JSON Expressions
 	@Test (groups = { "Groovy", "Search_Text_Lands" },priority=3)
-	public void extractMapOfObjectWithFindAllAndFindUsingParameters() {
+	public void extractMapOfObjectWithFindAllAndFindUsingParameters() 
+	{
+		test = extent.createTest("Create Test extractMapOfObjectWithFindAllAndFindUsingParameters");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in extractMapOfObjectWithFindAllAndFindUsingParameters test case", ExtentColor.BLUE));
 	    Response response = get("http://services.groupkt.com/country/search?text=lands");
 	    String code_2 = "NL";
 	    String code_3 = "NLD";
@@ -128,7 +132,9 @@ public class GroovyFeatures
 	
 	@Test (groups = { "Groovy", "Search_Text_Lands" },priority=3)
 	public void extractMapOfElementsWithFind()
-	{	    
+	{	
+		test = extent.createTest("Create Test extractMapOfElementsWithFind");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in extractMapOfElementsWithFind test case", ExtentColor.BLUE));
 	    Response response = get("search?text=lands");
 	    Map<String,?> allTeamDataForSingleTeam = response.path("RestResponse.result.find { it.name == 'Netherlands' }");
 	    System.out.println(allTeamDataForSingleTeam);
@@ -144,7 +150,9 @@ public class GroovyFeatures
 	//Extract Single Value with Max and Min
 	@Test (groups = { "Groovy", "Football" },priority=1)
 	public void extractSingleValueWithHighestValueOfOtherElement()
-	{	    
+	{	
+		test = extent.createTest("Create Test extractSingleValueWithHighestValueOfOtherElement");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in extractSingleValueWithHighestValueOfOtherElement test case", ExtentColor.BLUE));
 	    Response response = get("http://api.football-data.org/v1/teams/66/players");
 	    String highestNumberPlayer = response.path("players.max { it.jerseyNumber }.name");
 	    System.out.println(highestNumberPlayer); //max
@@ -156,6 +164,8 @@ public class GroovyFeatures
 	@Test (groups = { "Groovy", "Search_Text_Lands" },priority=1)
 	public void testGETResponseAsList()
 	{
+		test = extent.createTest("Create Test testGETResponseAsList");
+		test.log(Status.INFO,MarkupHelper.createLabel("I am in testGETResponseAsList test case", ExtentColor.BLUE));
 		String resp = get("search?text=lands").asString();
 		List<String> ls = from(resp).getList("RestResponse.result.name");
 		System.out.println("List Size := "+ls.size());
@@ -167,4 +177,6 @@ public class GroovyFeatures
 			} 
 		}
 	}
+	
+	
 }
